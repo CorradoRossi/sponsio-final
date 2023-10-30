4a660f24ee82222a9ccf57eb1dca09b33fe529f0
Rossi Meacham <rossimeacham@gmail.com>
2023-10-30 17:03:42 -0400

    try this

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -34,9 +34,10 @@ export const SocketProvider = ({
     const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
       path: "/api/socket/io",
       addTrailingSlash: false,
+      reconnection: false,
       skipReconnect: true,
       reconnectionDelay: 10000,
-      reconnectionAttempts: 10,
+      reconnectionAttempts: 5,
     });
 
     socketInstance.on("connect", () => {
